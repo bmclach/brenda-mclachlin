ae031ed68cbde00935067a655ad96c6f28c2d982
bmclach <77411753+bmclach@users.noreply.github.com>
2021-04-05 10:34:28 -0400

    Create contact-form-handler.php

---
+++ contact-form-handler.php
@@ -0,0 +1,45 @@
+$errors = '';
+$myemail = 'bmclach3@gmail.com'
+if(empty($_POST['name'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+$message = $_POST['message']; 
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+
+{
+
+$to = $myemail;
+
+$email_subject = "$name sent you an email!"
+
+$email_body = "Someone wants to know about piano lessons! Yay!!!"
+
+" Here are the details:\n Name: $name \n ".
+
+"Email: $email_address\n Message \n $message";
+
+$headers = "From: $myemail\n";
+
+$headers .= "Reply-To: $email_address";
+
+mail($to,$email_subject,$email_body,$headers);
+
+//redirect to the 'thank you' page
+
+header('Location: contact-form-thank-you.html');
+
+}
